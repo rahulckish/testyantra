3e7a1f37c014aadd5c7346fd9b6fb3714410be69
USER <USER@DELL>
2021-02-04 23:36:52 +0530

    push

---
+++ JenkinsProj/src/main/java/jenkinProject/JenkinsProj/App.java
@@ -0,0 +1,13 @@
+package jenkinProject.JenkinsProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
